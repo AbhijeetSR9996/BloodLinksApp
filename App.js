56b87cd2adbde8567aa2cb672046f49cea68bc3b
AbhijeetSR9996 <sabhijeet191@yahoo.com>
2023-05-31 17:26:23 +0530

    Samar

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { LogBox } from 'react-native';
+import StackNavigator from './src/navigation/StackNavigator';
+import { Provider as PaperProvider } from 'react-native-paper';
+import AsyncStorage from "@react-native-async-storage/async-storage";npx 
+
+const App = () => {
+  LogBox.ignoreAllLogs(true)
+  useEffect(() => {
+    Name()
+
+  }, []);
+  const Name = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('Gender')
+    } catch (e) {
+      // error
+    }
+  }
+  return (
+    <PaperProvider>
+      <StackNavigator />
+    </PaperProvider>
+  );
+};
+export default App;
+
+
+
